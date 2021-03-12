7b364ca8063266e73ef9046e354d094f49ee8fb8
anhpt1 <anhpt1@runsystem.net>
2021-03-12 15:53:56 +0700

    Init project

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Ecommerce
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
+  end
+end
