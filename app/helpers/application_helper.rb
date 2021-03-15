3cdbfdedda76f058ce130bfd368a2b631b39e4b6
anhpt1 <anhpt1@runsystem.net>
2021-03-15 17:16:21 +0700

    Fix Init Layout 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def full_title (page_title = '')
-    base_title = t('hnanaut')
+  def full_title(page_title = '')
+    base_title = t('logo')
     page_title.blank? ? base_title : "#{page_title} | #{base_title}"
   end
 end
