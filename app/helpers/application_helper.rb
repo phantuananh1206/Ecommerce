8c15e946df4a3308c2e06f2f4add07f70dca95c3
anhpt1 <anhpt1@runsystem.net>
2021-04-08 17:36:03 +0700

    Fix Admin manage list products

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,16 @@ module ApplicationHelper
     options = { method: :patch, remote: :true, class: 'btn btn-success btn-md' }
     link_to(text, admin_order_path(id: order.id, status: event_name), options)
   end
+
+  def get_all_categories
+    Category.all
+  end
+
+  def get_all_brands
+    Brand.all
+  end
+
+  def get_total_products
+    Product.all.size
+  end
 end
